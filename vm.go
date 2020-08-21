389af6bf82041da524af0db6d4f3f291eb0ccd1f
electricface <songwentai@uniontech.com>
2020-08-21 15:13:06 +0800

    perf: 提前启动核心组件
    需要等待前端优化完毕后才能合并，比如出现桌面壁纸空白，
    dock显示不出来。
    
    Log: 提前启动核心组件
    Bug: https://pms.uniontech.com/zentao/bug-view-47995.html
    
    Change-Id: Ia0adf1b04e8fbd6a352ef9f23517b50fec30173c
    Reviewed-on: http://gerrit.uniontech.com/c/startdde/+/4214
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: songwentai <songwentai@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- vm.go
+++ vm.go
@@ -26,32 +26,23 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/linuxdeepin/go-dbus-factory/com.deepin.daemon.display"
 	"github.com/godbus/dbus"
+	display "github.com/linuxdeepin/go-dbus-factory/com.deepin.daemon.display"
 	"pkg.deepin.io/lib/utils"
 	"pkg.deepin.io/lib/xdg/basedir"
 )
 
-func tryMatchVM() {
-	inVM, err := isInVM()
-	if err != nil {
-		logger.Warning("launchWindowManager detect VM failed:", err)
-		return
-	}
-
-	if !inVM {
-		return
-	}
-
-	logger.Debug("launchWindowManager in VM")
+func maybeLaunchWMChooser() (launched bool) {
+	logger.Debug("launch WMChooser in VM")
 	cfgFile := filepath.Join(basedir.GetUserConfigDir(), "deepin", "deepin-wm-switcher", "config.json")
 	if _, err := os.Stat(cfgFile); os.IsNotExist(err) {
 		err := exec.Command("dde-wm-chooser", "-c", cfgFile).Run()
-		globalWmChooserLaunched = true
+		launched = true
 		if err != nil {
 			logger.Warning(err)
 		}
 	}
+	return
 }
 
 func correctVMResolution() {
